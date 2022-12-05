c2ab18e6be64d5e87c32da5d05df48a672f9f9e2
Nicole Haraj <nicoleharaj@outlook.com>
2022-12-05 12:40:49 -0500

    configure purgeCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import Sidebar from '../components/Sidebar';
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <div className='flex flex-col w-full h-screen xl:flex-row'>
+    <div className='font-sans flex flex-col w-full h-screen xl:flex-row'>
 
       <nav className='border-b bg-stone-100 transition dark:bg-stone-800 dark:border-stone-600 xl:w-80 xl:h-screen xl:border-r' >
         <Sidebar />
--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,11 @@
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+];
+
 module.exports = {
-  plugins: ['postcss-import', 'tailwindcss', 'autoprefixer'],
+  plugins: ['postcss-import', 'tailwindcss', 'autoprefixer', ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])],
 };
